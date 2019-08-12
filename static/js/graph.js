9b3d23ed1e5038e0e1f15ac34fedc8d7ef081769
Ubuntu <ubuntu@ip-172-31-19-171.eu-west-1.compute.internal>
2019-08-12 14:06:01 +0000

    second chart renders,NaN error to be fixed

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,14 +1,19 @@
 queue()
     .defer(d3.csv, "data/game_highs_stats.csv")
+    .defer(d3.csv, "data/salaries.csv")
     .await(makeGraphs);
 
-function makeGraphs(error, game_stats) {
+function makeGraphs(error, game_stats, salaries) {
     var ndx = crossfilter(game_stats);
+    var ndx1 = crossfilter(salaries);
+    
     show_highest_points(ndx);
+    show_salaries(ndx1);
     
     dc.renderAll();
 
 }
+
 function show_highest_points(ndx) {
     var playerColors = d3.scale.ordinal()
         .domain(["Michael Jordan", "Kobe Bryant", "Lebron James"])
@@ -31,6 +36,7 @@ function show_highest_points(ndx) {
         .symbolSize(8)
         .clipPadding(10)
         .xAxisLabel("Season")
+        .yAxisLabel("Points")
         .title(function (d) {
             return d.key[2] + ' ' + d.key[1];
         })
@@ -42,3 +48,40 @@ function show_highest_points(ndx) {
         .group(seasonPlayerGroup)
         .margins({ top: 10, right: 50, bottom: 75, left: 75 });
 }
+
+function show_salaries(ndx1){
+    var playerColors=d3.scale.ordinal()
+    .domain(["Micheal Jordan","Kobe Bryant", "Lebron James"])
+    .range (["red", "blue", "green"]);
+
+    var eDim= ndx1.dimension(dc.pluck("Season"));
+    var seasonDim= ndx1.dimension(function(d){
+        
+        return [new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.Salary), d.Player];
+
+    })
+    var seasonPlayerGroup= seasonDim.group();
+    var minSeason= eDim.bottom(1)[0].Season;
+    var maxSeason= eDim.top(1)[0].Season;
+
+    dc.scatterPlot("#salary-per-season")
+    .width(800)
+        .height(400)
+        .x(d3.time.scale().domain([new Date(minSeason, 0, 1), new Date(maxSeason, 0, 1)]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .xAxisLabel("Season")
+        .yAxisLabel("Salary USD")
+        .title(function(d) {
+            return d.key[2] + d.key[1];
+        })
+        .colorAccessor(function (d) {
+            return d.key[2];
+        })
+        .colors(playerColors)
+        .dimension(seasonDim)
+        .group(seasonPlayerGroup)
+        .margins({top: 10, right: 50, bottom: 75, left: 75});
+} 
+
