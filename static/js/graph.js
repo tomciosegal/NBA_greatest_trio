eaa40ae818f2e4824ce6787c7e19efdd59cd1b52
Ubuntu <ubuntu@ip-172-31-19-171.eu-west-1.compute.internal>
2019-08-15 10:42:42 +0000

    third chart is rendering fine

--- static/js/graph.js
+++ static/js/graph.js
@@ -18,7 +18,7 @@ function makeGraphs(error, game_stats, salaries, totals) {
 
 }
 
-function show_player_selector(ndx){
+function show_player_selector(ndx,ndx1, ndx2){
     var dim = ndx.dimension(dc.pluck('Player'));
     var group = dim.group();
     
@@ -62,23 +62,23 @@ function show_highest_points(ndx) {
         .margins({ top: 10, right: 50, bottom: 75, left: 75 });
 }
 
-function show_salaries(ndx1){
-    var playerColors=d3.scale.ordinal()
-    .domain(["Micheal Jordan","Kobe Bryant", "Lebron James"])
-    .range (["red", "blue", "green"]);
+function show_salaries(ndx1) {
+    var playerColors = d3.scale.ordinal()
+        .domain(["Michael Jordan", "Kobe Bryant", "Lebron James"])
+        .range(["red", "blue", "green"]);
 
-    var eDim= ndx1.dimension(dc.pluck("Salary"));
-    var salaryDim= ndx1.dimension(function(d){
-        
-        return [new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.Salary), d.Player];
+    var eDim = ndx1.dimension(dc.pluck("Season"));
+    var salaryDim = ndx1.dimension(function (d) {
+        console.log([new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.Salary.slice(1)), d.Player])
+        return [new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.Salary.slice(1)), d.Player];
 
     })
-    var salaryPlayerGroup= salaryDim.group();
-    var minSeason  = eDim.bottom(1)[0].Age;
-    var maxSeason= eDim.top(1)[0].Age;
+    var salaryPlayerGroup = salaryDim.group();
+    var minSeason = eDim.bottom(1)[0].Season.slice(0, -3);
+    var maxSeason = eDim.top(1)[0].Season.slice(0, -3);
 
     dc.scatterPlot("#salary-vs-season")
-    .width(800)
+        .width(800)
         .height(400)
         .x(d3.time.scale().domain([new Date(minSeason, 0, 1), new Date(maxSeason, 0, 1)]))
         .brushOn(false)
@@ -86,8 +86,8 @@ function show_salaries(ndx1){
         .clipPadding(10)
         .xAxisLabel("Season")
         .yAxisLabel("Salary USD")
-        .title(function(d) {
-            return d.key[2] + d.key[1];
+        .title(function (d) {
+            return d.key[2] + ' ' + d.key[1];
         })
         .colorAccessor(function (d) {
             return d.key[2];
@@ -95,8 +95,8 @@ function show_salaries(ndx1){
         .colors(playerColors)
         .dimension(salaryDim)
         .group(salaryPlayerGroup)
-        .margins({top: 10, right: 50, bottom: 75, left: 75});
-} 
+        .margins({ top: 10, right: 50, bottom: 75, left: 75 });
+}
 
  function show_regular_season_points_vs_playoffs(ndx2) {
             var name_dim = ndx2.dimension(dc.pluck('Player'));
