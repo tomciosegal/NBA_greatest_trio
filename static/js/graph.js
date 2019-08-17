05644b3ab6eb7b7c033d3dcab2ff406f96a75a13
Ubuntu <ubuntu@ip-172-31-19-171.eu-west-1.compute.internal>
2019-08-17 16:57:04 +0000

    all line charts are working fine

--- static/js/graph.js
+++ static/js/graph.js
@@ -9,16 +9,53 @@ function makeGraphs(error, game_stats, salaries, totals) {
     var ndx1 = crossfilter(salaries);
     var ndx2 = crossfilter(totals);
     
+      totals.forEach((d, index) => {
+          d.Season = new Date(Number(d.Season.slice(0, -3)), 0, 1);
+    })
+    var i =0;
+    var j = 0;
+    var k = 0;
+    totalsForMJ = [];
+    totalsForKB = [];
+    totalsForLJ = [];
+    totals.forEach((d, index) => {
+          if (d.Player === 'Michael Jordan') {
+              totalsForMJ[i] = d;
+              i = i + 1;
+          }
+          else if (d.Player === 'Kobe Bryant') {
+              totalsForKB[j] = d;
+              j = j + 1;
+          }
+          else  {
+              totalsForLJ[k] = d;
+              k = k + 1;
+          }
+    });
+    
     show_player_selector(ndx,ndx1);
     show_highest_points(ndx);
     show_salaries(ndx1);
     show_regular_season_points_vs_playoffs(ndx2);
     show_teams_played(ndx2);
-    show_points_over_career(ndx2);
+    show_salary_made_in_each_team(ndx1)
+    show_points_over_career_MJ(totalsForMJ);
+    show_points_over_career_KB(totalsForKB);
+    show_points_over_career_LJ(totalsForLJ);
+    showTotalNumberOfGamesPlayedMJ(totalsForMJ);
+    showTotalNumberOfGamesPlayedKB(totalsForKB);
+    showTotalNumberOfGamesPlayedLJ(totalsForLJ);
     
     dc.renderAll();
 
 }
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
+
 
 function show_player_selector(ndx,ndx1, ndx2){
     var dim = ndx.dimension(dc.pluck('Player'));
@@ -38,7 +75,7 @@ function show_highest_points(ndx) {
 
     var eDim = ndx.dimension(dc.pluck("Season"));
     var seasonDim = ndx.dimension(function (d) {
-        return [new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.PTS), d.Player];
+        return [new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.PTS), (d.Player)];
 
     })
     var seasonPlayerGroup = seasonDim.group();
@@ -55,7 +92,7 @@ function show_highest_points(ndx) {
         .xAxisLabel("Season")
         .yAxisLabel("Points")
         .title(function (d) {
-            return d.key[2] + ' ' + d.key[1];
+            return d.key[2] + ': ' + d.key[1] + ' Points';
         })
         .colorAccessor(function (d) {
             return d.key[2];
@@ -72,7 +109,7 @@ function show_salaries(ndx1) {
         .range(["red", "blue", "green"]);
     var eDim = ndx1.dimension(dc.pluck("Season"));
     var salaryDim = ndx1.dimension(function (d) {
-        console.log(typeof d.Season);
+        
         return [new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.Salary.slice(1)), d.Player];
 
     })
@@ -90,7 +127,7 @@ function show_salaries(ndx1) {
         .xAxisLabel("Season")
         .yAxisLabel("Salary USD")
         .title(function (d) {
-            return d.key[2] + ' ' + d.key[1];
+            return d.key[2] + ' ' + formatter.format(d.key[1]);
         })
         .colorAccessor(function (d) {
             return d.key[2];
@@ -168,25 +205,77 @@ function show_salaries(ndx1) {
         .margins({top: 10, right: 100, bottom: 30, left: 30});
 }
 
- 
+ function show_salary_made_in_each_team(ndx1) {
+     
+     function teamByPlayers(dimension, team) {
+        return dimension.group().reduce(
+            function (p, v) {
+                p.total++;
+                
+                if(v.Tm == team) {
+                    p.match++;
+                }
+                return p;
+            },
+            function (p, v) {
+                p.total--;
+                if(v.Tm == team) {
+                    p.match--;
+                }
+                return p;
+            },
+            function () {
+                return {total: 0, match: 0};
+            }
+        );
+    }
+    
+    var dim = ndx1.dimension(dc.pluck("Player"));
+    
+    var chi = teamByPlayers(dim, "CHI");
+    var cle = teamByPlayers(dim, "CLE");
+    var lal = teamByPlayers(dim, "LAL");
+    var mia = teamByPlayers(dim, "MIA");
+    var was = teamByPlayers(dim, "WAS");
+    
+    dc.barChart("#salary_made_in_each_team")
+        .width(400)
+        .height(300)
+        .dimension(dim)
+        .group(chi, "CHI")
+        .stack(cle, "CLE")
+        .stack(lal, "LAL")
+        .stack(mia, "MIA")
+        .stack(was, "WAS")
+        .valueAccessor(function(d) {
+            if(d.value.total > 0) {
+                return parseFloat(parseFloat((d.value.match / d.value.total) * 100).toFixed(2));
+            } else {
+                return 0;
+            }
+        })
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
+        .margins({top: 10, right: 100, bottom: 30, left: 30});
+}
 
-function show_points_over_career(ndx2) {
+function show_points_over_career_MJ(totalsForMJ) {
+
+    
+    var ndx2 = crossfilter(totalsForMJ);
+    
     let dim = ndx2.dimension(dc.pluck("Season"));
-    let mj = dim.group().reduceSum(dc.pluck("Michael Jordan"));
-    let kb = dim.group().reduceSum(dc.pluck("Kobe Bryant"));
-    let lj = dim.group().reduceSum(dc.pluck("Lebron James"));
+    let ast = dim.group().reduceSum(dc.pluck("AST"));
+    let stl = dim.group().reduceSum(dc.pluck("STL"));
+    let blk = dim.group().reduceSum(dc.pluck("BLK"));
 
-
-    var seasonDim = ndx2.dimension(function (d) {
-        
-          return [new Date(Number(d.Season.slice(0, -3)), 0, 1), d.PTS, d.Player];
-    })
     // var seasonPlayerGroup = seasonDim.group();
     
-    let minSeason = dim.bottom(1)[0].Season.slice(0, -3);
-    let maxSeason = dim.top(1)[0].Season.slice(0, -3);
+    let minSeason = dim.bottom(1)[0].Season;
+    let maxSeason = dim.top(1)[0].Season;
 
-    let composite = dc.compositeChart("#show_points_over_career");
+    let composite = dc.compositeChart("#show_points_over_career_MJ");
 
     composite
         .width(840)
@@ -217,7 +306,7 @@ function show_points_over_career(ndx2) {
                 .attr("fill", "#E5E5E5");
             chart.selectAll(".dc-legend-item text")
                 .attr("font-size", "15px")
-                .attr("fill", "#ffffff");
+                .attr("fill", "black");
             chart.selectAll("line")
                 .style("stroke", "#E5E5E5");
             chart.selectAll(".domain")
@@ -227,31 +316,128 @@ function show_points_over_career(ndx2) {
         })
         .compose([
             dc.lineChart(composite)
-            .group(mj, "Michael")
+            .group(ast, "Assist")
             .interpolate("monotone")
             .title(function(d) {
                 let numberWithCommas = d.value.toLocaleString();
-                return numberWithCommas + " points";
+                return numberWithCommas + " assists";
             })
             .colors("#ff7e0e")
             .dotRadius(10)
             .renderDataPoints({ radius: 4 }),
             dc.lineChart(composite)
             .interpolate("monotone")
-            .group(kb, "Kobe")
+            .group(stl, "Steal")
             .title(function(d) {
                 let numberWithCommas = d.value.toLocaleString();
-                return numberWithCommas + " points";
+                return numberWithCommas + " steals";
             })
             .colors("#d95350")
             .dotRadius(10)
             .renderDataPoints({ radius: 4 }),
             dc.lineChart(composite)
-            .group(lj, "Lebron")
+            .group(blk, "Block")
             .interpolate("monotone")
             .title(function(d) {
                 let numberWithCommas = d.value.toLocaleString();
-                // return numberWithCommas + "points";
+                return numberWithCommas + "blocks";
+            })
+            .colors("#1e77b4")
+            .dotRadius(10)
+            .renderDataPoints({ radius: 4 })
+        ])
+        .brushOn(false)
+        .yAxisPadding("5%")
+        .elasticX(true)
+        .xAxisPadding("8%")
+        .xAxis().ticks(12).tickFormat(d3.time.format("%b")).outerTickSize(0);
+
+    composite.yAxis().ticks(5).outerTickSize(0);
+}
+
+
+
+function show_points_over_career_KB(totalsForKB) {
+
+    
+    var ndx2 = crossfilter(totalsForKB);
+    
+    let dim = ndx2.dimension(dc.pluck("Season"));
+    let ast = dim.group().reduceSum(dc.pluck("AST"));
+    let stl = dim.group().reduceSum(dc.pluck("STL"));
+    let blk = dim.group().reduceSum(dc.pluck("BLK"));
+
+    // var seasonPlayerGroup = seasonDim.group();
+    
+    let minSeason = dim.bottom(1)[0].Season;
+    let maxSeason = dim.top(1)[0].Season;
+
+    let composite = dc.compositeChart("#show_points_over_career_KB");
+
+    composite
+        .width(840)
+        .height(310)
+        .margins({ top: 10, right: 60, bottom: 50, left: 45 })
+        .dimension(dim)
+        .elasticY(true)
+        .legend(dc.legend().x(230).y(320).itemHeight(15).gap(5)
+            .horizontal(true).itemWidth(100))
+        .x(d3.time.scale().domain([new Date(minSeason, 0, 1), new Date(maxSeason, 0, 1)]))
+        .y(d3.scale.linear())
+        .transitionDuration(500)
+        .shareTitle(false)
+        .on("renderlet", (function(chart) {
+            chart.selectAll(".dot")
+                .style("cursor", "pointer");
+        }))
+        .on("pretransition", function(chart) {
+            chart.selectAll("g.y text")
+                .style("font-size", "12px");
+            chart.selectAll("g.x text")
+                .style("font-size", "12px");
+            chart.select("svg")
+                .attr("height", "100%")
+                .attr("width", "100%")
+                .attr("viewBox", "0 0 840 340");
+            chart.selectAll(".dc-chart text")
+                .attr("fill", "#E5E5E5");
+            chart.selectAll(".dc-legend-item text")
+                .attr("font-size", "15px")
+                .attr("fill", "black");
+            chart.selectAll("line")
+                .style("stroke", "#E5E5E5");
+            chart.selectAll(".domain")
+                .style("stroke", "#E5E5E5");
+            chart.selectAll(".line")
+                .style("stroke-width", "2.5");
+        })
+        .compose([
+            dc.lineChart(composite)
+            .group(ast, "Assist")
+            .interpolate("monotone")
+            .title(function(d) {
+                let numberWithCommas = d.value.toLocaleString();
+                return numberWithCommas + " assists";
+            })
+            .colors("#ff7e0e")
+            .dotRadius(10)
+            .renderDataPoints({ radius: 4 }),
+            dc.lineChart(composite)
+            .interpolate("monotone")
+            .group(stl, "Steal")
+            .title(function(d) {
+                let numberWithCommas = d.value.toLocaleString();
+                return numberWithCommas + " steals";
+            })
+            .colors("#d95350")
+            .dotRadius(10)
+            .renderDataPoints({ radius: 4 }),
+            dc.lineChart(composite)
+            .group(blk, "Block")
+            .interpolate("monotone")
+            .title(function(d) {
+                let numberWithCommas = d.value.toLocaleString();
+                return numberWithCommas + "blocks";
             })
             .colors("#1e77b4")
             .dotRadius(10)
@@ -269,8 +455,141 @@ function show_points_over_career(ndx2) {
 
 
 
+
+
+
+function show_points_over_career_LJ(totalsForLJ) {
+
+    
+    var ndx2 = crossfilter(totalsForLJ);
+    
+    let dim = ndx2.dimension(dc.pluck("Season"));
+    let ast = dim.group().reduceSum(dc.pluck("AST"));
+    let stl = dim.group().reduceSum(dc.pluck("STL"));
+    let blk = dim.group().reduceSum(dc.pluck("BLK"));
+
+    // var seasonPlayerGroup = seasonDim.group();
+    
+    let minSeason = dim.bottom(1)[0].Season;
+    let maxSeason = dim.top(1)[0].Season;
+
+    let composite = dc.compositeChart("#show_points_over_career_LJ");
+
+    composite
+        .width(840)
+        .height(310)
+        .margins({ top: 10, right: 60, bottom: 50, left: 45 })
+        .dimension(dim)
+        .elasticY(true)
+        .legend(dc.legend().x(230).y(320).itemHeight(15).gap(5)
+            .horizontal(true).itemWidth(100))
+        .x(d3.time.scale().domain([new Date(minSeason, 0, 1), new Date(maxSeason, 0, 1)]))
+        .y(d3.scale.linear())
+        .transitionDuration(500)
+        .shareTitle(false)
+        .on("renderlet", (function(chart) {
+            chart.selectAll(".dot")
+                .style("cursor", "pointer");
+        }))
+        .on("pretransition", function(chart) {
+            chart.selectAll("g.y text")
+                .style("font-size", "12px");
+            chart.selectAll("g.x text")
+                .style("font-size", "12px");
+            chart.select("svg")
+                .attr("height", "100%")
+                .attr("width", "100%")
+                .attr("viewBox", "0 0 840 340");
+            chart.selectAll(".dc-chart text")
+                .attr("fill", "#E5E5E5");
+            chart.selectAll(".dc-legend-item text")
+                .attr("font-size", "15px")
+                .attr("fill", "black");
+            chart.selectAll("line")
+                .style("stroke", "#E5E5E5");
+            chart.selectAll(".domain")
+                .style("stroke", "#E5E5E5");
+            chart.selectAll(".line")
+                .style("stroke-width", "2.5");
+        })
+        .compose([
+            dc.lineChart(composite)
+            .group(ast, "Assist")
+            .interpolate("monotone")
+            .title(function(d) {
+                let numberWithCommas = d.value.toLocaleString();
+                return numberWithCommas + " assists";
+            })
+            .colors("#ff7e0e")
+            .dotRadius(10)
+            .renderDataPoints({ radius: 4 }),
+            dc.lineChart(composite)
+            .interpolate("monotone")
+            .group(stl, "Steal")
+            .title(function(d) {
+                let numberWithCommas = d.value.toLocaleString();
+                return numberWithCommas + " steals";
+            })
+            .colors("#d95350")
+            .dotRadius(10)
+            .renderDataPoints({ radius: 4 }),
+            dc.lineChart(composite)
+            .group(blk, "Block")
+            .interpolate("monotone")
+            .title(function(d) {
+                let numberWithCommas = d.value.toLocaleString();
+                return numberWithCommas + "blocks";
+            })
+            .colors("#1e77b4")
+            .dotRadius(10)
+            .renderDataPoints({ radius: 4 })
+        ])
+        .brushOn(false)
+        .yAxisPadding("5%")
+        .elasticX(true)
+        .xAxisPadding("8%")
+        .xAxis().ticks(12).tickFormat(d3.time.format("%b")).outerTickSize(0);
+
+    composite.yAxis().ticks(5).outerTickSize(0);
+}
     
 
  
+function showTotalNumberOfGamesPlayedMJ(totalsForMJ) {
+    let ndx2 = crossfilter(totalsForMJ);
+    let dim = ndx2.dimension(dc.pluck("Player"));
+    let totalGames = dim.group().reduceSum(dc.pluck("G"));
 
+    dc.numberDisplay("#totalNumberOfGamesPlayedMJ")
+        .formatNumber(d3.format(",.0f"))
+        .group(totalGames)
+        .valueAccessor(function(d) {
+            return (d.value);
+        });
+}
 
+function showTotalNumberOfGamesPlayedKB(totalsForKB) {
+    let ndx2 = crossfilter(totalsForKB);
+    let dim = ndx2.dimension(dc.pluck("Player"));
+    let totalGames = dim.group().reduceSum(dc.pluck("G"));
+
+    dc.numberDisplay("#totalNumberOfGamesPlayedKB")
+        .formatNumber(d3.format(",.0f"))
+        .group(totalGames)
+        .valueAccessor(function(d) {
+            return (d.value);
+        });
+}
+
+function showTotalNumberOfGamesPlayedLJ(totalsForLJ) {
+    let ndx2 = crossfilter(totalsForLJ);
+    let dim = ndx2.dimension(dc.pluck("Player"));
+    let totalGames = dim.group().reduceSum(dc.pluck("G"));
+
+    dc.numberDisplay("#totalNumberOfGamesPlayedLJ")
+        .formatNumber(d3.format(",.0f"))
+        .group(totalGames)
+        .valueAccessor(function(d) {
+            return (d.value);
+        });
+}
