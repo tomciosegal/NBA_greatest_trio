38daa663a9353cdf9f3cc669f86ef2acaf3e261c
Ubuntu <ubuntu@ip-172-31-19-171.eu-west-1.compute.internal>
2019-09-06 09:25:07 +0000

    fotter fixed for small devices

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,32 +8,32 @@ function makeGraphs(error, game_stats, salaries, totals) {
     var ndx = crossfilter(game_stats);
     var ndx1 = crossfilter(salaries);
     var ndx2 = crossfilter(totals);
-    
-      totals.forEach((d, index) => {
-          d.Season = new Date(Number(d.Season.slice(0, -3)), 0, 1);
+
+    totals.forEach((d, index) => {
+        d.Season = new Date(Number(d.Season.slice(0, -3)), 0, 1);
     })
-    var i =0;
+    var i = 0;
     var j = 0;
     var k = 0;
     totalsForMJ = [];
     totalsForKB = [];
     totalsForLJ = [];
     totals.forEach((d, index) => {
-          if (d.Player === 'Michael Jordan') {
-              totalsForMJ[i] = d;
-              i = i + 1;
-          }
-          else if (d.Player === 'Kobe Bryant') {
-              totalsForKB[j] = d;
-              j = j + 1;
-          }
-          else  {
-              totalsForLJ[k] = d;
-              k = k + 1;
-          }
+        if (d.Player === 'Michael Jordan') {
+            totalsForMJ[i] = d;
+            i = i + 1;
+        }
+        else if (d.Player === 'Kobe Bryant') {
+            totalsForKB[j] = d;
+            j = j + 1;
+        }
+        else {
+            totalsForLJ[k] = d;
+            k = k + 1;
+        }
     });
-    
-    show_player_selector(ndx,ndx1);
+
+    show_player_selector(ndx, ndx1);
     show_highest_points(ndx);
     show_salaries(ndx1);
     show_total_points(ndx2);
@@ -44,27 +44,27 @@ function makeGraphs(error, game_stats, salaries, totals) {
     showTotalNumberOfGamesPlayedMJ(totalsForMJ);
     showTotalNumberOfGamesPlayedKB(totalsForKB);
     showTotalNumberOfGamesPlayedLJ(totalsForLJ);
-    
+
     dc.renderAll();
 
 }
 
 const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
 })
 
 
-function show_player_selector(ndx,ndx1, ndx2){
+function show_player_selector(ndx, ndx1, ndx2) {
     var dim = ndx.dimension(dc.pluck('Player'));
     var group = dim.group();
     var dim1 = ndx1.dimension(dc.pluck('Player'));
     var group1 = dim1.group();
-    
+
     dc.selectMenu("#player-selector")
-    .dimension(dim)
-    .group(group)
+        .dimension(dim)
+        .group(group)
 }
 
 function show_highest_points(ndx) {
@@ -73,7 +73,7 @@ function show_highest_points(ndx) {
         .range(["#e22b22bf", "#ffe854b5", "#13a3547d"]);
 
     var eDim = ndx.dimension(dc.pluck("Season"));
-    var seasonDim = ndx.dimension(function (d) {
+    var seasonDim = ndx.dimension(function(d) {
         return [new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.PTS), (d.Player)];
 
     })
@@ -90,10 +90,11 @@ function show_highest_points(ndx) {
         .clipPadding(10)
         .xAxisLabel("Season")
         .yAxisLabel("Points")
-        .title(function (d) {
+        .title(function(d) {
             return d.key[2] + ': ' + d.key[1] + ' Points';
         })
-        .colorAccessor(function (d) {
+
+        .colorAccessor(function(d) {
             return d.key[2];
         })
         .colors(playerColors)
@@ -107,8 +108,8 @@ function show_salaries(ndx1) {
         .domain(["Michael Jordan", "Kobe Bryant", "Lebron James"])
         .range(["#e22b22bf", "#ffe854b5", "#13a3547d"]);
     var eDim = ndx1.dimension(dc.pluck("Season"));
-    var salaryDim = ndx1.dimension(function (d) {
-        
+    var salaryDim = ndx1.dimension(function(d) {
+
         return [new Date(Number(d.Season.slice(0, -3)), 0, 1), Number(d.Salary.slice(1)), d.Player];
 
     })
@@ -125,10 +126,10 @@ function show_salaries(ndx1) {
         .clipPadding(10)
         .xAxisLabel("Season")
         .yAxisLabel("Salary USD")
-        .title(function (d) {
+        .title(function(d) {
             return d.key[2] + ' ' + formatter.format(d.key[1]);
         })
-        .colorAccessor(function (d) {
+        .colorAccessor(function(d) {
             return d.key[2];
         })
         .colors(playerColors)
@@ -137,36 +138,37 @@ function show_salaries(ndx1) {
         .margins({ top: 10, right: 50, bottom: 75, left: 75 });
 }
 
- function show_total_points(ndx2) {
-            var name_dim = ndx2.dimension(dc.pluck('Player'));
-            var total_points_per_player = name_dim.group().reduceSum(dc.pluck('PTS'));
-            var playerColors = d3.scale.ordinal()
-                .domain(["Michael Jordan", "Kobe Bryant", "Lebron James"])
-                .range(["#e22b22bf", "#ffe854b5", "#13a3547d"]);
-               
-                
-                
-            dc.pieChart('#points-season-playoffs')
-                .height(540)
-                .radius(400)
-                .colors(playerColors)
-                .transitionDuration(1500)
-                .dimension(name_dim)
-                .group(total_points_per_player)
-                .title(function (d) {
+function show_total_points(ndx2) {
+    var name_dim = ndx2.dimension(dc.pluck('Player'));
+    var total_points_per_player = name_dim.group().reduceSum(dc.pluck('PTS'));
+    var playerColors = d3.scale.ordinal()
+        .domain(["Michael Jordan", "Kobe Bryant", "Lebron James"])
+        .range(["#e22b22bf", "#ffe854b5", "#13a3547d"]);
+
+
+
+    dc.pieChart('#points-season-playoffs')
+        .height(540)
+        .radius(400)
+        .colors(playerColors)
+        .transitionDuration(1500)
+        .dimension(name_dim)
+        .group(total_points_per_player)
+        
+        .title(function(d) {
             return d.key + ': ' + d.value + ' Points';
         });
-                
-                
- }
- 
+
+
+}
+
 //  function show_teams_played(ndx2) {
-     
+
 //      function teamByPlayers(dimension, team) {
 //         return dimension.group().reduce(
 //             function (p, v) {
 //                 p.total++;
-                
+
 //                 if(v.Tm == team) {
 //                     p.match++;
 //                 }
@@ -184,15 +186,15 @@ function show_salaries(ndx1) {
 //             }
 //         );
 //     }
-    
+
 //     var dim = ndx2.dimension(dc.pluck("Player"));
-    
+
 //     var chi = teamByPlayers(dim, "CHI");
 //     var cle = teamByPlayers(dim, "CLE");
 //     var lal = teamByPlayers(dim, "LAL");
 //     var mia = teamByPlayers(dim, "MIA");
 //     var was = teamByPlayers(dim, "WAS");
-    
+
 //     dc.barChart("#show_teams_played")
 //         .width(550)
 //         .height(450)
@@ -216,42 +218,42 @@ function show_salaries(ndx1) {
 // }
 
 
- function show_teams_played(ndx2) {
-     
-     function teamByPlayers(dimension, player) {
+function show_teams_played(ndx2) {
+
+    function teamByPlayers(dimension, player) {
         return dimension.group().reduce(
-            function (p, v) {
+            function(p, v) {
                 p.total++;
-                
-                if(v.Player == player) {
+
+                if (v.Player == player) {
                     p.match++;
                 }
                 return p;
             },
-            function (p, v) {
+            function(p, v) {
                 p.total--;
-                if(v.Player == player) {
+                if (v.Player == player) {
                     p.match--;
                 }
                 return p;
             },
-            function () {
-                return {total: 0, match: 0};
+            function() {
+                return { total: 0, match: 0 };
             }
         );
     }
-    
-     var playerColors = d3.scale.ordinal()
-                .domain(["Michael Jordan", "Kobe Bryant", "Lebron James"])
-                .range(["#e22b22bf", "#ffe854b5", "#13a3547d"]);
-    
+
+    var playerColors = d3.scale.ordinal()
+        .domain(["Michael Jordan", "Kobe Bryant", "Lebron James"])
+        .range(["#e22b22bf", "#ffe854b5", "#13a3547d"]);
+
     var dim = ndx2.dimension(dc.pluck("Tm"));
-    
+
     var mj = teamByPlayers(dim, "Michael Jordan");
     var kb = teamByPlayers(dim, "Kobe Bryant");
     var lj = teamByPlayers(dim, "Lebron James");
-    
-    
+
+
     dc.barChart("#show_teams_played")
         .width(550)
         .height(450)
@@ -260,31 +262,43 @@ function show_salaries(ndx1) {
         .group(mj, "Micheal Jordan")
         .stack(kb, "Kobe Bryant")
         .stack(lj, "Lebron James")
-        .valueAccessor(function(d) {
-            if(d.value.total > 0) {
+        
+
+
+    .valueAccessor(function(d) {
+            if (d.value.total > 0) {
                 return parseFloat(parseFloat((d.value.match / d.value.total) * 100).toFixed(2));
-            } else {
+            }
+            else {
                 return 0;
             }
         })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
-        .margins({top: 10, right: 100, bottom: 30, left: 30});
+        .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5)
+        )
+        
+        .margins({ top: 10, right: 100, bottom: 30, left: 30 })
+        
+
+    ;
+
+
+
 }
 
 function show_main_stats_career_MJ(totalsForMJ) {
 
-    
+
     var ndx2 = crossfilter(totalsForMJ);
-    
+
     let dim = ndx2.dimension(dc.pluck("Season"));
     let ast = dim.group().reduceSum(dc.pluck("AST"));
     let stl = dim.group().reduceSum(dc.pluck("STL"));
     let blk = dim.group().reduceSum(dc.pluck("BLK"));
 
     // var seasonPlayerGroup = seasonDim.group();
-    
+
     let minSeason = dim.bottom(1)[0].Season;
     let maxSeason = dim.top(1)[0].Season;
 
@@ -363,7 +377,7 @@ function show_main_stats_career_MJ(totalsForMJ) {
         .yAxisPadding("5%")
         .elasticX(true)
         .xAxisPadding("8%")
-        
+
 
     composite.yAxis().ticks(5).outerTickSize(0);
 }
@@ -372,16 +386,16 @@ function show_main_stats_career_MJ(totalsForMJ) {
 
 function show_main_stats_career_KB(totalsForKB) {
 
-    
+
     var ndx2 = crossfilter(totalsForKB);
-    
+
     let dim = ndx2.dimension(dc.pluck("Season"));
     let ast = dim.group().reduceSum(dc.pluck("AST"));
     let stl = dim.group().reduceSum(dc.pluck("STL"));
     let blk = dim.group().reduceSum(dc.pluck("BLK"));
 
     // var seasonPlayerGroup = seasonDim.group();
-    
+
     let minSeason = dim.bottom(1)[0].Season;
     let maxSeason = dim.top(1)[0].Season;
 
@@ -460,7 +474,7 @@ function show_main_stats_career_KB(totalsForKB) {
         .yAxisPadding("5%")
         .elasticX(true)
         .xAxisPadding("8%")
-        
+
 
     composite.yAxis().ticks(5).outerTickSize(0);
 }
@@ -473,16 +487,16 @@ function show_main_stats_career_KB(totalsForKB) {
 
 function show_main_stats_career_LJ(totalsForLJ) {
 
-    
+
     var ndx2 = crossfilter(totalsForLJ);
-    
+
     let dim = ndx2.dimension(dc.pluck("Season"));
     let ast = dim.group().reduceSum(dc.pluck("AST"));
     let stl = dim.group().reduceSum(dc.pluck("STL"));
     let blk = dim.group().reduceSum(dc.pluck("BLK"));
 
     // var seasonPlayerGroup = seasonDim.group();
-    
+
     let minSeason = dim.bottom(1)[0].Season;
     let maxSeason = dim.top(1)[0].Season;
 
@@ -561,17 +575,17 @@ function show_main_stats_career_LJ(totalsForLJ) {
         .yAxisPadding("5%")
         .elasticX(true)
         .xAxisPadding("8%")
-        
+
 
     composite.yAxis().ticks(5).outerTickSize(0);
 }
-    
 
- 
-        function showTotalNumberOfGamesPlayedMJ(totalsForMJ) {
-        let ndx2 = crossfilter(totalsForMJ);
-        let dim = ndx2.dimension(dc.pluck("Player"));
-        let totalGames = dim.group().reduceSum(dc.pluck("G"));
+
+
+function showTotalNumberOfGamesPlayedMJ(totalsForMJ) {
+    let ndx2 = crossfilter(totalsForMJ);
+    let dim = ndx2.dimension(dc.pluck("Player"));
+    let totalGames = dim.group().reduceSum(dc.pluck("G"));
 
     dc.numberDisplay("#totalNumberOfGamesPlayedMJ")
         .formatNumber(d3.format(",.0f"))
